732cef2b62f1df2aef79f5a721115589a53e3b67
Dave Corley <smokydave@icloud.com>
2024-02-05 17:10:53 -0600

    Clean up logs, save each map in its own directory, and split up object faces according to their texture. This means we also have switched to always using root collision

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,14 @@ use shalrath::repr::*;
 use shambler::{
     brush::BrushId,
     entity::EntityId,
-    face::{FaceNormals, FaceTriangleIndices, FaceUvs, FaceVertices},
+    face::{FaceId, FaceNormals, FaceTriangleIndices, FaceUvs, FaceVertices},
     texture::{TextureId, TextureSizes},
     GeoMap, Textures, TexturesTag, Vector2 as SV2, Vector3 as SV3,
 };
 use std::{
     collections::{BTreeMap, HashMap, HashSet},
     env, fs,
+    io::Write,
     path::PathBuf,
 };
 use tes3::{
@@ -173,12 +174,10 @@ impl MapData {
 
     fn find_textures_in_vfs(textures: &HashSet<String>) -> HashSet<String> {
         let config = get_config().expect("Openmw.cfg not detected! Please ensure you have a valid openmw configuration file in the canonical system directory.");
-        let texture_paths: HashSet<String> = textures
+        textures
             .iter()
             .filter_map(|texture_name| MapData::find_vfs_texture(&texture_name, &config))
-            .collect();
-
-        texture_paths
+            .collect()
     }
 
     fn get_entity_properties(&self, entity_id: &EntityId) -> HashMap<&String, &String> {
@@ -238,20 +237,23 @@ impl Mesh {
 
     fn from_map(brushes: &Vec<BrushId>, map_data: &MapData) -> Mesh {
         let mut mesh = Mesh::new();
+        mesh.attach_collision();
 
         // Every brush should be a unique TriShape
         // However, faces which use multiple textures must be split further
         // Perhaps we store the texture string and the vertex data in a hashmap,
         // keyed against the texture string
         for brush_id in brushes {
-            let node = BrushNiNode::from_brush(brush_id, map_data);
+            let brush_nodes = BrushNiNode::from_brush(brush_id, map_data);
 
-            if node.col_verts.len() != node.vis_verts.len() && !mesh.use_collision_root {
-                println!("Enabling root collision from on brush {brush_id}");
-                mesh.attach_collision();
+            for node in brush_nodes {
+                // if node.col_verts.len() != node.vis_verts.len() && !mesh.use_collision_root {
+                //     println!("Enabling root collision from on brush {brush_id}");
+                //     mesh.attach_collision();
+                // }
+
+                mesh.attach_node(node, &map_data.texture_paths);
             }
-
-            mesh.attach_node(node, &map_data.texture_paths);
         }
         mesh
     }
@@ -277,22 +279,18 @@ impl Mesh {
             };
         }
 
-        if self.use_collision_root == true {
-            // We need to figure out how to omit the collision node, should it be identical to vis data
+        if node.col_verts.len() > 0 {
+            let col_index = self.stream.insert(node.col_shape);
 
-            if node.col_verts.len() > 0 {
-                let col_index = self.stream.insert(node.col_shape);
+            let col_data_index = self.stream.insert(node.col_data);
 
-                let col_data_index = self.stream.insert(node.col_data);
+            if let Some(collision) = self.stream.get_mut(col_index) {
+                collision.geometry_data = col_data_index.cast();
+            };
 
-                if let Some(collision) = self.stream.get_mut(col_index) {
-                    collision.geometry_data = col_data_index.cast();
-                };
-
-                if let Some(collision_root) = self.stream.get_mut(self.collision_index) {
-                    collision_root.children.push(col_index.cast());
-                };
-            }
+            if let Some(collision_root) = self.stream.get_mut(self.collision_index) {
+                collision_root.children.push(col_index.cast());
+            };
         }
     }
 
@@ -309,7 +307,7 @@ impl Mesh {
             let candidate_path = format!("Textures/{file_path}.{extension_candidate}");
             if let Ok(rel_path) = find_file(&config, candidate_path.as_str()) {
                 extension = extension_candidate.to_string();
-                continue;
+                break;
             }
         }
 
@@ -335,30 +333,47 @@ struct BrushNiNode {
     vis_data: NiTriShapeData,
     vis_verts: Vec<SV3>,
     vis_tris: Vec<Vec<usize>>,
-    texture: String,
     normals: Vec<SV3>,
+    texture: String,
+    uv_sets: Vec<SV2>,
     col_shape: NiTriShape,
     col_data: NiTriShapeData,
     col_verts: Vec<SV3>,
     col_tris: Vec<Vec<usize>>,
-    uv_sets: Vec<SV2>,
 }
 
 impl BrushNiNode {
-    fn from_brush(brush_id: &BrushId, map_data: &MapData) -> Self {
+    fn from_brush(brush_id: &BrushId, map_data: &MapData) -> Vec<BrushNiNode> {
+        // let mut node = BrushNiNode::default();
+        // let faces = map_data.geomap.brush_faces.get(brush_id).unwrap();
+
+        let mut face_nodes = Vec::new();
+
+        let faces_with_textures = Self::collect_faces_with_textures(brush_id, map_data);
+
+        for face_set in faces_with_textures {
+            face_nodes.push(Self::node_from_faces(&face_set, &map_data));
+        }
+
+        for node in &mut face_nodes {
+            node.collect()
+        }
+
+        face_nodes
+    }
+
+    fn node_from_faces(faces: &Vec<FaceId>, map_data: &MapData) -> BrushNiNode {
         let mut node = BrushNiNode::default();
-        let faces = map_data.geomap.brush_faces.get(brush_id).unwrap();
 
         for face_id in faces.iter() {
             let texture_id = map_data.geomap.face_textures.get(face_id).unwrap();
             let texture_name = map_data.geomap.textures.get(texture_id).unwrap();
 
-            if texture_name == "skip" {
+            if texture_name == "skip" || texture_name.contains("skip") {
                 continue;
             };
-            // Before moving onto texture generation,
 
-            let s_flags = match &map_data.geomap.face_extensions.get(face_id) {
+            let mut s_flags = match &map_data.geomap.face_extensions.get(face_id) {
                 Some(Extension::Quake2 {
                     content_flags: _,
                     surface_flags,
@@ -370,12 +385,24 @@ impl BrushNiNode {
             let vertices = &map_data.face_vertices.get(&face_id).unwrap();
 
             // Later, we'll need to do something extra on the brush to define whether it can be inverted
-            let indices = match map_data.face_tri_indices.get(&face_id) {
-                Some(indices) => indices,
+            let mut indices = match map_data.face_tri_indices.get(&face_id) {
+                Some(indices) => indices.clone(),
                 None => {
                     continue;
                 }
             };
+
+            // Test for water or slime types
+            if texture_name.to_ascii_lowercase().contains("slime")
+                || texture_name.to_ascii_lowercase().contains("water")
+                || texture_name.to_ascii_lowercase().contains("lava")
+                || texture_name.to_ascii_lowercase().contains("mwat")
+            {
+                let inverted_indices = map_data.inverted_face_tri_indices.get(&face_id).unwrap();
+                indices.extend_from_slice(inverted_indices);
+                s_flags |= (NiBroomSurface::NoClip as u32);
+                println!("{face_id} interpreted as liquid")
+            }
 
             let uv_sets = &map_data.face_uvs.get(&face_id).unwrap();
 
@@ -395,16 +422,50 @@ impl BrushNiNode {
                 node.col_tris.push((*indices).to_vec());
             }
         }
-
-        node.collect();
         node
+    }
+
+    fn collect_faces_with_textures(brush_id: &BrushId, map_data: &MapData) -> Vec<Vec<FaceId>> {
+        let mut face_textures = Vec::new();
+
+        let faces = map_data.geomap.brush_faces.get(brush_id).unwrap();
+
+        for face in faces.iter() {
+            let texture_id = map_data.geomap.face_textures.get(face).unwrap();
+            let texture_name = map_data.geomap.textures.get(texture_id).unwrap();
+            if !face_textures.contains(texture_name) {
+                face_textures.push(texture_name.to_string())
+            }
+        }
+
+        let mut faces_with_matching_textures: Vec<Vec<FaceId>> =
+            vec![Vec::new(); face_textures.len()];
+
+        for (index, texture) in face_textures.iter().enumerate() {
+            for face in faces.iter() {
+                let texture_id = map_data.geomap.face_textures.get(face).unwrap();
+                let texture_name = map_data.geomap.textures.get(texture_id).unwrap();
+
+                if texture_name == texture {
+                    faces_with_matching_textures[index].push(*face);
+                }
+            }
+        }
+
+        // println!("{face_textures:?}");
+
+        // for texture_set in &faces_with_matching_textures {
+        //     println!("Texture set: {texture_set:?}")
+        // }
+
+        faces_with_matching_textures
     }
 
     /// Don't forget to deal with collision and attachment!!!!
     fn from_brushes(brushes: &[BrushId], map_data: &MapData) -> Vec<BrushNiNode> {
         brushes
             .iter()
-            .map(|brush_id| BrushNiNode::from_brush(brush_id, map_data))
+            .flat_map(|brush_id| BrushNiNode::from_brush(brush_id, map_data))
             .collect()
     }
 
@@ -460,13 +521,13 @@ impl Default for BrushNiNode {
             vis_data: NiTriShapeData::default(),
             vis_verts: Vec::new(),
             vis_tris: Vec::new(),
-            texture: String::new(),
             normals: Vec::new(),
+            texture: String::new(),
+            uv_sets: Vec::new(),
             col_shape: NiTriShape::default(),
             col_data: NiTriShapeData::default(),
             col_verts: Vec::new(),
             col_tris: Vec::new(),
-            uv_sets: Vec::new(),
         }
     }
 }
@@ -482,15 +543,20 @@ fn main() {
         ),
     };
 
+    let ext_index = map_name
+        .rfind('.')
+        .expect("Map should always have an extension, something is very wrong");
+    let workdir = &map_name[..ext_index];
+
+    if !fs::metadata(workdir).is_ok() {
+        fs::create_dir(workdir).expect("Folder creation failed! This is very bad!");
+        println!("Folder '{}' created.", workdir);
+    }
+
     let map_data = MapData::new(map_name);
     let mut processed_base_objects: Vec<String> = Vec::new();
 
     for (entity_id, brushes) in map_data.geomap.entity_brushes.iter() {
-        // So what happens, right?
-        // Well, if we identify that this specific entity is a group, we need to gather all its children.
-        // If that object is a group, skip it. If it's not a group,
-        // Add the NiNodes to the mesh
-
         let prop_map = map_data.get_entity_properties(entity_id);
 
         let mut mesh = Mesh::from_map(brushes, &map_data);
@@ -505,7 +571,7 @@ fn main() {
                 }
             }
             None => {
-                println!("This object has no refid, and isn't part of a group. It may be the worldspawn?");
+                // println!("This object has no refid, and isn't part of a group. It may be the worldspawn?");
             }
         }
 
@@ -531,12 +597,12 @@ fn main() {
                     match prop_map.get(&"_tb_group".to_string()) {
                         Some(obj_group) => {
                             if obj_group != group_id {
-                                println!("Found another group! Bailing on creating this mesh and saving it into the cellref.");
+                                // println!("Found another group! Bailing on creating this mesh and saving it into the cellref.");
                                 continue;
                             };
                         }
                         None => {
-                            println!("This object isn't part of a group, don't do anything with it here.");
+                            // println!("This object isn't part of a group, don't do anything with it here.");
                             continue;
                         }
                     }
@@ -560,10 +626,6 @@ fn main() {
                 }
 
                 for node in nodes {
-                    if node.col_verts.len() != node.vis_verts.len() && !mesh.use_collision_root {
-                        mesh.attach_collision();
-                    }
-
                     mesh.attach_node(node, &map_data.texture_paths);
                 }
             }
@@ -571,6 +633,6 @@ fn main() {
         }
 
         // Every entity is its own mesh
-        mesh.save(format!("test_{entity_id}.nif"));
+        mesh.save(format!("{workdir}/test_{entity_id}.nif"));
     }
 }
