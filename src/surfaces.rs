8cd2099d64b263cfc9d632085d336c47621fe834
Dave Corley <smokydave@icloud.com>
2024-02-08 14:14:36 -0600

    Use arrays instead of tuples for constants

--- src/surfaces.rs
+++ src/surfaces.rs
@@ -1,10 +1,10 @@
 pub const MAP_SCALE: f32 = 2.;
 
 pub mod colors {
-    pub const SKY: (f32, f32, f32) = (1., 0., 1.);
-    pub const WATER: (f32, f32, f32) = (0., 0., 1.);
-    pub const LAVA: (f32, f32, f32) = (1., 0., 0.);
-    pub const SLIME: (f32, f32, f32) = (0., 1., 0.);
+    pub const SKY: [f32; 3] = [1., 0., 1.];
+    pub const WATER: [f32; 3] = [0., 0., 1.];
+    pub const LAVA: [f32; 3] = [1., 0., 0.];
+    pub const SLIME: [f32; 3] = [0., 1., 0.];
 }
 
 pub enum NiBroomSurface {
