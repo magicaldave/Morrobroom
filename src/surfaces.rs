d3401ac9116ca4313399ef193fffd483012acb0b
Dave Corley <smokydave@icloud.com>
2024-02-26 18:30:26 -0600

    Feat(surfaces): Add default book string, make more configurable later

--- src/surfaces.rs
+++ src/surfaces.rs
@@ -1,5 +1,8 @@
 pub const MAP_SCALE: f32 = 2.;
 
+pub const BOOK_START_DEFAULT: &str =
+    "<DIV ALIGN=\"LEFT\"><FONT COLOR=\"000000\" SIZE=\"3\" FACE=\"Magic Cards\"><BR>";
+
 pub mod colors {
     pub const SKY: [f32; 3] = [1., 0., 1.];
     pub const WATER: [f32; 3] = [0., 0., 1.];
